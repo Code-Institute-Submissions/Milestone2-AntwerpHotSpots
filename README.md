b248327f30986dfa706a9dd53616ee7cabd274f0
ZahraSadiq <zsadiq27@gmail.com>
2020-10-11 19:40:34 +0000

    Integrated JS Google Maps API

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Flaticon Reference: for logo
 Rule for attribution: 
 Icons made by <a href="https://www.flaticon.com/authors/eucalyp" title="Eucalyp">Eucalyp</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+
+1/3 % 2/3 colums from: https://courses.codeinstitute.net/courses/course-v1:codeinstitute+FE+2017_T3/courseware/616289d66b5641a3808cc43e53842695/a701aa4956df4ce8a1026791c248d5c9/?child=first 
---
+++ assets/js/maps.js
@@ -0,0 +1,23 @@
+function initMap() {
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 12,
+    center: {
+      lat: 51.219448,
+      lng: 4.402464,
+    },
+  });
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var locations = [
+        { lat: 51.21712, lng: 4.400812 },
+    ];
+
+    var markers = locations.map(function (location, i) {
+        return new google.maps.Marker({
+        position: location,
+        label: labels[i % labels.length],
+        });
+    });
+
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m" });
+    }
