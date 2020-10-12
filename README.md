ede168d6ff042d549f9827e6790d7ad785c7040e
ZahraSadiq <zsadiq27@gmail.com>
2020-10-12 22:17:33 +0000

    Started creating custom markers to google maps

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ Rule for attribution:
 Icons made by <a href="https://www.flaticon.com/authors/eucalyp" title="Eucalyp">Eucalyp</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
 
 1/3 % 2/3 colums from: https://courses.codeinstitute.net/courses/course-v1:codeinstitute+FE+2017_T3/courseware/616289d66b5641a3808cc43e53842695/a701aa4956df4ce8a1026791c248d5c9/?child=first 
+
+Tutorial for Google Maps:
+https://courses.codeinstitute.net/courses/course-v1:CodeInstitute+IFD101+2017_T3/courseware/03d3f6524ad249d9b33e3336d156dfd0/3b2af8636ea54a4d9dc45126f7498633/?activate_block_id=block-v1%3ACodeInstitute%2BIFD101%2B2017_T3%2Btype%40sequential%2Bblock%403b2af8636ea54a4d9dc45126f7498633 
+
+flaticon attribute:
+<div>Icons made by <a href="https://www.flaticon.local/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.local/" title="Flaticon">www.flaticon.local</a></div>
--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,11 +1,87 @@
-function initMap() {
-  var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 12,
-    center: {
-      lat: 51.219448,
-      lng: 4.402464,
-    },
-  });
+var map;
+var centerCords = {
+    lat: 51.219448,
+    lng: 4.402464,
+};
+
+ var icons = {
+          single: {
+            icon: '/assets/images/mapIcons/map_marker_test.png'
+          },
+          birthday: {
+            icon: '/assets/images/mapIcons/map_marker_test.png'
+          },
+          couple: {
+            icon: '/assets/images/mapIcons/map_marker_test.png'
+          },
+          junkFood: {
+            icon: '/assets/images/mapIcons/map_marker_test.png'
+          },
+          brunch: {
+            icon: '/assets/images/mapIcons/map_marker_test.png'
+          },
+          surprise: {
+            icon: '/assets/images/mapIcons/map_marker_test.png'
+          }
+        };
+
+        var markersOnMap = [
+            {
+                LatLng: [{
+                    lat: 51.221183,
+                    lng: 4.40409
+                }],
+                type: 'single'
+            },
+            {
+                LatLng: [{
+                    lat: 51.216689,
+                    lng: 4.412087
+                }],
+                type: 'single'
+            },
+            {
+                LatLng: [{
+                    lat: 51.20002,
+                    lng: 4.414494
+                }],
+                type: 'single'
+            },
+            {
+                LatLng: [{
+                    lat: 51.234057,
+                    lng: 4.408796
+                }],
+                type: 'single'
+            },
+            {
+                LatLng: [{
+                    lat: 51.213176,
+                    lng: 4.394185
+                }],
+                type: 'single'
+            }
+        ];
+
+        function addMarkerInfo() {
+            for (var i = 0; i < markersOnMap.length; i++ ) {
+                var marker = new google.maps.Marker ({
+                    position: markersOnMap[i].LatLng[0],
+                    icon: icons[markersOnMap[i].type].icon,
+                    map: map
+                });
+            };
+        }
+
+        function initMap() {
+            map = new google.maps.Map(document.getElementById("map"), {
+                zoom: 12,
+                center: centerCords
+            });
+            addMarkerInfo();
+        }
+
+  /*
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     var locations = [
@@ -20,4 +96,4 @@ function initMap() {
     });
 
     var markerCluster = new MarkerClusterer(map, markers, { imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m" });
-    }
+    } */
